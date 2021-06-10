bb91425f457e62e8025027a23c783bff00db90af
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-06-10 12:08:59 -0400

    write questions and basic markdown

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
-
+const { rejects } = require('assert');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { resolve } = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is your project's name?"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a description of your project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 2.0', 'BSD 2.0', 'None']
+    },
+    {
+        type: 'list',
+        name: 'dependencies',
+        message: 'What command should be run to install dependencies?',
+        choices: ['npm install inquire']
+    },
+    {
+        type: 'list',
+        name: 'tests',
+        message: 'What command should be run to run tests?',
+        choices: ['npm test']
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?'
+    }
+];
+
+inquirer
+.prompt(questions)
+.then(data => console.log(data))
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,8 +14,46 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+  *[Installation](#installation)
+  *[Usage](#Usage)
+  *[License](#License)
+  *[Tests](#Tests)
+  *[Contributing](#Contributing)
+
+  ## Installation
+
+  To install the necessary dependenciesrun the following command:
+  ${data.dependencies}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  This product is licensed under the ${data.license} license.
+
+  ## Tests
+
+  To run tests, run the following command:
+  
+  ${data.tests}
+
+  ## Contributing
+
+  ${data.contributing}
+
+
+  ## Questions 
+  If you ahve any questions about the repo, open an issue, or contact me directly at ${data.email}. You can find more of my work at ${data.username}.
 `;
 }
 
